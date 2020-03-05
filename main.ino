18b93526c72a470a222fab688409c05fc4a2ad4b
Pontus Aurdal <pontus.edvard@gmail.com>
2020-03-05 23:02:13 +0100

    fix: add modified working CoAP library and change modem setup code

--- main.ino
+++ main.ino
@@ -1,7 +1,7 @@
 /*
- * Arduino Dev-Kit UDP Example Code.
+ * Arduino Dev-Kit CoAP Example Code.
  * 
- * The example sends UDP data packets over LTE-M (Cat M1)
+ * The example sends CoAP data packets over LTE-M (Cat M1)
  * or NB-IoT network to Telenor Managed IoT Cloud (MIC).
  * 
  * For help and other code examples:
@@ -10,100 +10,89 @@
 
 #include <MKRNB.h>
 
+#include "coap.h"
 #include "arduino_secrets.h"
 
 // Enter sensitive data and other configurations in "arduino_secrets.h".
-const char   pinnumber[]      = SECRET_PINNUMBER;
-const char   gprs_apn[]       = SECRET_GPRS_APN;
-unsigned int udp_port         = SECRET_UDP_PORT;
-unsigned int rat              = SECRET_RAT;
-unsigned int cops             = SECRET_COPS;
+char     pinnumber[]     = SECRET_PINNUMBER;
+char     gprs_apn[]      = SECRET_GPRS_APN;
+uint32_t coap_ip         = SECRET_COAP_IP;
+char     coap_endpoint[] = SECRET_COAP_ENDPOINT;
+uint32_t coap_port       = SECRET_COAP_PORT;
+uint32_t rat             = SECRET_RAT;
+uint32_t cops            = SECRET_COPS;
+bool     debug           = true;
 
-// Telenor IoT Gateway IP address
-IPAddress iotgw_ip(172, 16, 15, 14);
-byte packetBuffer[512];
-
-NB nbAccess;
-GPRS gprs;
-NBUDP Udp;
+NB        nbAccess(debug);
+GPRS      gprsAccess;
+NBUDP     Udp;
+Coap      coap(Udp);
+IPAddress iotgw_ip(coap_ip);
 
 void setup() {
-  String response;
-
-  // Reset the ublox module
-  pinMode(SARA_RESETN, OUTPUT);
-  digitalWrite(SARA_RESETN, HIGH);
-  delay(100);
-  digitalWrite(SARA_RESETN, LOW);
-
   // Open serial communication and wait for port to open
   Serial.begin(115200);
   while (!Serial);
 
-  // Wait for modem to get ready
-  Serial.println("Waiting for modem to get ready...");
+  /**
+   * Issue a series of AT commands to the modem.
+   * 
+   *    - Wait for it to get ready
+   *    - Disconnect from any connected networks
+   *    - Set RAT (NB-IoT or LTE-M)
+   *    - Apply changes and save configuration
+   *    - Turn radio on
+   *    - Check attachment
+   *    - Set operator
+   */
+
+  Serial.print("Waiting for modem to get ready...");
   MODEM.begin();
   while (!MODEM.noop());
   Serial.println("done.");
 
-  // Disconnect from any networks
   Serial.print("Disconnecting from network...");
-  MODEM.sendf("AT+COPS=2");
-  MODEM.waitForResponse(2000, &response);
-  Serial.println(response);
+  MODEM.send("AT+COPS=2");
+  MODEM.waitForResponse(2000);
   Serial.println("done.");
 
-  // Set Radio Access Technology (RAT)
-  Serial.print("Set radio technology to NB-IoT or Cat-M1 (7 is for LTE-M and 8 is for NB-IoT)...");
-  MODEM.sendf("AT+URAT=%s", rat);
-  MODEM.waitForResponse(100, &response);
-  Serial.println(response);
+  Serial.print("Setting Radio Access Technology to NB-IoT or LTE-M (7 is for LTE-M and 8 is for NB-IoT)...");
+  MODEM.sendf("AT+URAT=%d", rat);
+  MODEM.waitForResponse(2000);
   Serial.println("done.");
 
-  // Apply changes
   Serial.print("Applying changes and saving configuration...");
-  MODEM.sendf("AT+CFUN=15");
-  MODEM.waitForResponse(5000);
-  delay(5000);
-
-  while (MODEM.waitForResponse(1000) != 1) {
+  MODEM.send("AT+CFUN=15");
+  do {
     delay(1000);
     MODEM.noop();
-  }
+  } while (MODEM.waitForResponse(1000) != 1);
   Serial.println("done.");
 
-  // Turn modem on
-  Serial.println("Modem ready, turn radio on in order to configure it...");
+  Serial.print("Modem ready, turn radio on in order to configure it...");
   MODEM.send("AT+CFUN=1");
-  MODEM.waitForResponse(2000, &response);
-  Serial.println(response);
+  MODEM.waitForResponse(2000);
   Serial.println("done.");
 
-  // Wait for a good signal strength (between 0 and 98)
-  Serial.println("Check attachment until CSQ RSSI indicator is less than 99...");
+  Serial.print("Check attachment until CSQ RSSI indicator is less than 99...");
+  String response;
   int status = 99;
   while (status > 98 && status > 0) {
     MODEM.send("AT+CSQ");
-    MODEM.waitForResponse(2000, &response);
-
+    MODEM.waitForResponse(2000);
     String sub = response.substring(6, 8);
     status = sub.toInt(); // Will return 0 if no valid number is found
     delay(1000);
   }
   Serial.println("done.");
 
-  // Set operator to Telenor
-  Serial.println("Set operator to Telenor...");
-  MODEM.sendf("AT+COPS=1,2,\"%s\"", cops);
-  MODEM.waitForResponse(2000, &response);
-  Serial.println(response);
+  Serial.print("Set operator to Telenor...");
+  MODEM.sendf("AT+COPS=1,2,\"%d\"", cops);
+  MODEM.waitForResponse(4000);
   Serial.println("done.");
 
-  // Set APN and check if network is ready
-  connectNB();
-
-  Serial.println("Setup socket for connection to MIC...");
-  Udp.begin(udp_port);
+  // Initialize CoAP client
+  coap.start();
 
   // Seed random number generator with noise from pin 0
   randomSeed(analogRead(0));
@@ -111,74 +100,59 @@ void setup() {
 
 void loop() {
   // Check if connected and if not, reconnect
-  if (nbAccess.status() != NB_READY || gprs.status() != GPRS_READY) {
+  if (nbAccess.status() != NB_READY || gprsAccess.status() != GPRS_READY) {
     connectNB();
   }
 
-  Serial.print("Send packet to MIC...");
+  Serial.print("Send packet to Telenor MIC...");
   sendPacket();
+  Serial.println("done.");
 
-  Serial.println("Check if we have received something...");
-  if (receivePacket() > 0) {
-    Serial.println("Received packet...");
-    String bufferString = String((char *) packetBuffer);
-    Serial.println("Packet data is: <" + bufferString + ">");
-  } else {
-    Serial.println("No data received...");
-  }
+  // Continue CoAP operations (handle ACK, send response)
+  coap.loop();
 
-  // Wait 30 seconds before sending again
-  Serial.println("Wait 30s before sending again...");
-  delay(30000);
+  Serial.println("Wait 10s before sending again...");
+  delay(10000);
 }
 
-void connectNB() {
-  Serial.println("Attempting to connect to the cellular network");
-
-  while ((nbAccess.begin(pinnumber) != NB_READY) ||
-         (gprs.attachGPRS(gprs_apn) != GPRS_READY)) {
-    // failed, retry
+bool connectNB() {
+  Serial.print("Attempting to connect to the cellular network...");
+  // Failed, retry
+  while (nbAccess.begin(pinnumber, gprs_apn) != NB_READY || gprsAccess.attachGPRS() != GPRS_READY) {
     Serial.print(".");
     delay(1000);
   }
+  Serial.println("done.");
 
-  Serial.println("You're connected to the cellular network!");
+  return true;
 }
 
-// Send a JSON formatted packet to MIC
-unsigned long sendPacket () {
-  String p1, p2, p4, payload = "";
-  float hum, tmp, r = 0.0;
+uint16_t sendPacket () {
+  // Generate random simulated data
+  float tmp = 20 + (random(0, 9) / 10);
+  float hum = 60 + (random(0, 9) / 10);
 
-  hum = 60;
-  r = random(0, 9);
-  r = r / 10;
-  hum = hum + r;
-  p1 = hum;
-  tmp = 20;
-  r = random(0, 9);
-  r = r / 10;
-  tmp = tmp + r;
-  p2 = tmp;
+  /**
+   * Create a JSON payload.
+   * You would typically compress the payload as much as possible here,
+   * and then use the "uplink transform" in the Thing Type to unpack it later
+   * once the packet has been received in Telenor MIC.
+   */
+  char buffer[100];
+  snprintf(buffer, 100, "{\"tmp\":\"%.2f\",\"hum\":\"%.2f\",\"latlng\":\"59.898812,10.627212\"}", tmp, hum);
 
-  payload = "{\"temperature\":\"" + p2 + "\", \"humidity\":\"" + p1 + "\",\"latlng\": \"69.681812, 18.988209\"}";
-  Serial.println("payload is: " + payload);
-  Udp.beginPacket(iotgw_ip, udp_port);
-  Udp.write(payload.c_str(), payload.length());
-  Udp.endPacket();
-}
+  // Send a CoAP POST message to Telenor IoT Gateway
+  uint16_t msgid = coap.send(
+    iotgw_ip,           // IP address
+    coap_port,          // Port
+    coap_endpoint,      // CoAP endpoint
+    COAP_CON,           // CoAP packet type (con, noncon, ack, reset)
+    COAP_POST,          // CoAP method
+    NULL,               // CoAP token
+    0,                  // CoAP token length
+    (uint8_t *) buffer, // Message payload
+    sizeof(buffer)      // Message payload length
+  );
 
-// Read a packet from the UDP port
-int receivePacket () {
-  int size = Udp.parsePacket();
-
-  // Check if size is larger than 0, if yes we have received something
-  if (size > 0) {
-    Serial.println("Packet received!");
-    Udp.read(packetBuffer, size); // Read the packet into the buffer
-
-    return size;
-  }
-
-  return 0;
+  return msgid;
 }
